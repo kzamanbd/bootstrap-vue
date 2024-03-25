bb8d877555e0aaeb46b92d72a6bb38f94a656865
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-03-26 04:34:20 +0600

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -33,5 +33,16 @@
                 <p class="my-4">Hello From My Modal!</p>
             </b-modal>
         </div>
+        <div class="d-flex gap-4 my-4">
+            <b-button v-b-modal.modal-xl variant="primary">xl modal</b-button>
+            <b-button v-b-modal.modal-lg variant="primary">lg modal</b-button>
+            <b-button v-b-modal.modal-sm variant="primary">sm modal</b-button>
+
+            <b-modal id="modal-xl" size="xl" title="Extra Large Modal">
+                Hello Extra Large Modal!
+            </b-modal>
+            <b-modal id="modal-lg" size="lg" title="Large Modal">Hello Large Modal!</b-modal>
+            <b-modal id="modal-sm" size="sm" title="Small Modal">Hello Small Modal!</b-modal>
+        </div>
     </div>
 </template>
